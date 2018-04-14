ae9462cd64327a95233f3c2f086f3fb8c56a69da
eppofahmi <eppofahmi@gmail.com.com>
2018-04-15 01:37:26 +0700

    add new repo

---
+++ Word2vec in R/word2vec-1.R
@@ -0,0 +1,9 @@
+# Mencoba rword2vec
+#---------------------------------------install---------------------
+
+library(devtools)
+library(rword2vec)
+
+# melihat list fungsi word2vec di R
+ls("package:rword2vec")
+
