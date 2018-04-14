7a625ab4313048a38e440b29f06a2165bfb7ed89
eppofahmi <eppofahmi@gmail.com.com>
2018-04-15 01:49:43 +0700

    first try

--- Word2vec in R/word2vec-1.R
+++ Word2vec in R/word2vec-1.R
@@ -1,9 +1,19 @@
 # Mencoba rword2vec
-#---------------------------------------install---------------------
+# Source: http://www.rpubs.com/mukul13/rword2vec
 
+#--------------------------Installing-----------------------------------------
 library(devtools)
 library(rword2vec)
 
 # melihat list fungsi word2vec di R
 ls("package:rword2vec")
+
+#-------------------------Training word2vec model------------------------------ 
+# To train text data to get word vectors:
+setwd("/Volumes/mydata/RStudio/belajaR/Word2vec in R")
+
+model <- word2vec(train_file = "text8", output_file = "vec.bin", binary=1)
+
+#-------------------------Distance-------------------------------------------- 
+# To get closest words:
 
